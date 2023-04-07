d68ef7bb8101093cd631a68be4e31d8296d53807
Facu <coronelfacundo30@gmail.com>
2023-04-07 20:32:05 -0300

    Installing angular material

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+   array = [1,2,3,4,5];
+}
+
