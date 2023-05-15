42023861438222c11ef9a098ecb70df34cef43b7
Facu <coronelfacundo30@gmail.com>
2023-05-15 11:00:15 -0300

    Update front-end

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactMeComponent } from './components/contact-me/contact-me.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
+  {path: 'projects', component: ProjectsComponent},
+  {path: 'contact-me', component: ContactMeComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactMeComponent } from './components/contact-me/contact-me.component';
 
 
 
@@ -14,6 +17,9 @@ import { HomeComponent } from './components/home/home.component';
     AppComponent,
     HeaderComponent,
     HomeComponent,
+    FooterComponent,
+    ProjectsComponent,
+    ContactMeComponent,
   ],
   imports: [
     BrowserModule,
